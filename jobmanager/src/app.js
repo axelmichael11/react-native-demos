c9cba604cc91d1f549ecd72fdf8e7a508b91c96a
Michael Axelson <axelmichael11@gmail.com>
2017-10-16 16:19:48 -0700

    authentication is good to go on the jobmanager thing

--- jobmanager/src/app.js
+++ jobmanager/src/app.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import firebase from 'firebase'
 import reducers from './reducers'
 import {APIKEY, AUTHDOMAIN, DATABASEURL, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID} from 'react-native-dotenv'
 import LoginForm from './components/loginform.js'
-
+import ReduxThunk from 'redux-thunk'
 class App extends Component {
   componentWillMount(){
     var config = {
@@ -21,7 +21,7 @@ class App extends Component {
   }
   render(){
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         <View>
           <LoginForm/>
         </View>
